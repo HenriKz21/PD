c8a056a26f82a5ac56cdf09d694478c85873b1f5
HenriKz21 <kyloh881@gmail.com>
2026-09-25 13:50:55 -0300

    Update microblog.py

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -3,11 +3,6 @@ from app.models.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    """
-    Registra instâncias no comando 'flask shell' para você
-    testar consultas e comandos no terminal sem precisar
-    importar 'db', 'User' e 'Post' manualmente toda vez.
-    """
     return {
         'db': db,
         'User': User,
